03daf2bf98a5f2a150ff80acca30eb5b809d5279
Min Khant Kyaw Swa <89757146+MKSuperCoder@users.noreply.github.com>
2024-01-29 21:54:17 -0500

    Add files via upload

---
+++ C++project/helloworld/Helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World \n";
+    cout << "Hello \n";
+    int age;
+    cout << "How old are you? ";
+    cin >> age;
+    cout << "I am " << age << " years old.";
+    return 0;
+}
+
